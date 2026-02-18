5004dfc38794eda83657235d9e1d5b68c94f9f3b
stupienius <wangjay0401@gmail.com>
2026-02-18 17:47:15 +0800

    beautify

--- terminal/command.js
+++ terminal/command.js
@@ -1,108 +1,81 @@
+// (class,href)<content>
 
- // (class,href)<content>
-
-const banner =[
-"                       _            _ ",           
-"       _              |_|          |_| ",          
-"   ___| |_ _   _ _ __  _  ___ _ __  _ _   _ ___", 
-"  / __| __| | | | '_ \\| |/ _ \\ '_ \\| | | | / __|",
-"  \\__ \\ |_| |_| | |_) | |  __/ | | | | |_| \\__ \\ ",
-"  |___/\\__|\\__,_| .__/|_|\\___|_| |_|_|\\__,_|___/",
-"                | |",                             
-"                |_|",
-"",
-"type (color1,)<help> for a list of available commands."                             
+const banner = [
+  "                       _            _ ",
+  "       _              |_|          |_| ",
+  "   ___| |_ _   _ _ __  _  ___ _ __  _ _   _ ___",
+  "  / __| __| | | | '_ \\| |/ _ \\ '_ \\| | | | / __|",
+  "  \\__ \\ |_| |_| | |_) | |  __/ | | | | |_| \\__ \\ ",
+  "  |___/\\__|\\__,_| .__/|_|\\___|_| |_|_|\\__,_|___/",
+  "                | |",
+  "                |_|",
+  "",
+  "type (color1,)<help> for a list of available commands.",
 ];
 
-const whoami =  [
-    "",
-    "Who are you?",
-    "", 
-    "    You are an ontological entity, a nexus of consciousness and temporal existence.",
-    "Your identity is a symbiotic amalgamation of genetic predispositions and experiential",
-    "phenomena, continuously morphing through dialectical interactions with the existential ",
-    "milieu. This dynamic essence is encapsulated in a phenomenological framework, where ",
-    "the self is perpetually reconstituted by subjective interpretations and intersubjective ",
-    "engagements. Thus, you exist as an ever-evolving construct, a confluence of being and ",
-    "becoming, navigating the liminal spaces of reality and perception."
+const whoami = [
+  "To define a human from a philosophical perspective is to go beyond mere biology and examine the qualities that distinguish humans as conscious and reflective beings. Humans are not only living organisms but also self-aware entities capable of thought, introspection, and deliberate action. Aristotle famously described humans as rational animals, emphasizing that reason allows us to navigate not only the physical world but also the ethical and social dimensions of life. This rationality is intertwined with our capacity for moral judgment: humans can discern between right and wrong, make choices guided by ethical principles, and reflect on the consequences of their actions. Yet, existentialist thinkers like Jean-Paul Sartre remind us that human existence is not predetermined; rather, we are beings defined by freedom, compelled to create our essence through choices, commitments, and projects. This freedom carries with it profound responsibility, as each decision contributes to the ongoing construction of the self. From a phenomenological perspective, humans are also defined by their lived experience—their conscious perception of the world, the meanings they assign to events, and their engagement with others. A human, therefore, is a being who inhabits both the objective reality of nature and the subjective realm of thought and emotion. We seek purpose, cultivate relationships, and grapple with questions of value, mortality, and identity. In this sense, humanity is not a fixed category but a dynamic process: humans are ever in the act of becoming, of interpreting, and of striving toward understanding. To be human is to dwell in the tension between the known and the unknown, the finite and the infinite, the individual and the collective—a creature of reflection, choice, and imagination, continually shaping both self and world.",
+  "",
+  "But wait.. you are Human ... right?",
 ];
 
 const who = [
-    
-    "Hello, I'm Stupienius, a high school student from Taiwan.",
-    "I'm continuously learning about information technology and",
-    "am particularly focused on mastering C, Python, and JavaScript.",
-    "These languages are essential for various aspects of software ",
-    "development, and I find the process of learning and applying them",
-    "both challenging and rewarding.",
-    "",
-    "Beyond my interest in programming, I genuinely enjoy making new",
-    "friends and connecting with people from different backgrounds. ",
-    "Building a network of like-minded individuals not only broadens ",
-    "my perspective but also provides opportunities for collaborative ",
-    "learning and growth.",
-    "",
-    "If you'd like to get to know me better or discuss our shared ",
-    "interests in technology, feel free to reach out. You can type ",
-    "(color1,)<contact> to get more information on how to connect with me. ",
-    "I'm always open to making new friends and exploring exciting ",
-    "new projects together."
+  "Hello, I'm Stupienius, a high school student from Taiwan.  I'm continuously learning about information technology and  am particularly focused on mastering C, Python, and JavaScript.  These languages are essential for various aspects of software   development, and I find the process of learning and applying them both challenging and rewarding.",
+  "",
+  "Beyond my interest in programming, I genuinely enjoy making new  friends and connecting with people from different backgrounds.   Building a network of like-minded individuals not only broadens   my perspective but also provides opportunities for collaborative   learning and growth.",
+  "",
+  "If you'd like to get to know me better or discuss our shared interests in technology, feel free to reach out. You can type (color1,)<contact> to get more information on how to connect with me. I'm always open to making new friends and exploring exciting new projects together.",
 ];
 
 const help = [
-    "(color1,)<help>           Show command helper",
-    "(color1,)<contact>        About my social contact",
-    "(color1,)<project>        Show my projects",
-    "(color1,)<who>            Who is stupienius",
-    "(color1,)<whoami>         Who are you",
-    "(color1,)<song>           Listen to music",
-    "(color1,)<game>           Good phone game recommend to you",
-    "(color1,)<banner>         Display the header",
-    "(color1,)<history>        Your command history",
-    "(color1,)<singer>         Best singer ever"
+  "(color1,)<help>           Show command helper",
+  "(color1,)<contact>        About my social contact",
+  "(color1,)<project>        Show my projects",
+  "(color1,)<who>            Who is stupienius",
+  "(color1,)<whoami>         Who are you",
+  "(color1,)<song>           Listen to music",
+  "(color1,)<game>           Good phone game recommend to you",
+  "(color1,)<banner>         Display the header",
+  "(color1,)<history>        Your command history",
+  "(color1,)<singer>         Best singer ever",
 ];
 
 const contact = [
-    "(color1,https://github.com/stupienius)<github>         github/stupienius",
-    "(color1,https://www.facebook.com/profile.php?id=100053935834116)<facebook>       facebook/Stupienius Null",
-    "(color1,https://www.instagram.com/stupienius/)<instagram>      instagram/stupienius",
-    "(color1,https://twitter.com/stupienius)<x>              x/stupienius"  
+  "(color1,https://github.com/stupienius)<github>         github/stupienius",
+  "(color1,https://www.facebook.com/profile.php?id=100053935834116)<facebook>       facebook/Stupienius Null",
+  "(color1,https://www.instagram.com/stupienius/)<instagram>      instagram/stupienius",
+  "(color1,https://twitter.com/stupienius)<x>              x/stupienius",
 ];
 
 const game = [
-    "    Programming is a highly valuable skill in today's job market.With a high demand", 
-    "for programmers, learning languages like Python,JavaScript, or C++ can significantly", 
-    "boost your career prospects.Unlike gaming, which provides temporary entertainment,", 
-    "programming equips you with practical skills for long-term success.",
-    "",
-    "    Programming also enhances critical thinking and problem-solving abilities.",
-    "Writing code involves logical reasoning, creativity, and persistence, skills",
-    "that are useful in various life aspects. In contrast, gaming often involves",
-    "repetitive tasks that don't contribute to personal development.",
-    "",
-    "    Moreover, programming encourages continuous learning. The tech industry constantly",
-    "evolves, motivating you to stay updated and improve your knowledge. This mindset of",
-    "lifelong learning is invaluable in our fast-paced world.",
-    "",
-    "    Lastly, programming empowers you to create and innovate. Unlike gaming, where you",
-    "consume content, programming allows you to build applications, websites, and more.", 
-    "This creative process is highly rewarding and can lead to a profound sense of accomplishment.",
-    "",
-    "    In summary, while gaming is fun, investing your time in programming can enhance your",
-    "career prospects, develop valuable skills, foster personal growth, and unlock your",
-    "creative potential. Redirect your time from gaming to programming and experience", 
-    "its transformative impact."
+  "    Programming is a highly valuable skill in today's job market.With a high demand",
+  "for programmers, learning languages like Python,JavaScript, or C++ can significantly",
+  "boost your career prospects.Unlike gaming, which provides temporary entertainment,",
+  "programming equips you with practical skills for long-term success.",
+  "",
+  "    Programming also enhances critical thinking and problem-solving abilities.",
+  "Writing code involves logical reasoning, creativity, and persistence, skills",
+  "that are useful in various life aspects. In contrast, gaming often involves",
+  "repetitive tasks that don't contribute to personal development.",
+  "",
+  "    Moreover, programming encourages continuous learning. The tech industry constantly",
+  "evolves, motivating you to stay updated and improve your knowledge. This mindset of",
+  "lifelong learning is invaluable in our fast-paced world.",
+  "",
+  "    Lastly, programming empowers you to create and innovate. Unlike gaming, where you",
+  "consume content, programming allows you to build applications, websites, and more.",
+  "This creative process is highly rewarding and can lead to a profound sense of accomplishment.",
+  "",
+  "    In summary, while gaming is fun, investing your time in programming can enhance your",
+  "career prospects, develop valuable skills, foster personal growth, and unlock your",
+  "creative potential. Redirect your time from gaming to programming and experience",
+  "its transformative impact.",
 ];
 
 const project = [
-    "go to (color1,https://github.com/stupienius)<github> to see my whole preject."
+  "go to (color1,https://github.com/stupienius)<github> to see my whole preject.",
 ];
 
-const song = [
-    "best song ever"
-]
+const song = ["best song ever"];
 
 let history = [];
-
-
-
--- terminal/ter.js
+++ terminal/ter.js
@@ -1,202 +1,199 @@
-const terminl = document.querySelector('.terminal');
+const terminl = document.querySelector(".terminal");
 let inputTextarea = document.querySelector("#text");
 
-
 let isAnimation = false;
-let newCommand,newInput,newOutput,container;
+let newCommand, newInput, newOutput, container;
 
-window.addEventListener("keydown",(e) => {
+window.addEventListener(
+  "keydown",
+  (e) => {
     window.scrollTo(0, document.body.offsetHeight);
     inputTextarea.focus();
-    inputTextarea.addEventListener('input', function () {
-    });
-},{passive:true});
+    inputTextarea.addEventListener("input", function() { });
+  },
+  { passive: true },
+);
 
 addNewCommandBlock();
 addParagraphAnimation(banner);
 
-
-function inputPrompt(textarea ,e){
-    if(isAnimation) return ;
-    if(/^[a-zA-Z]$/.test(e.key)){
-        newInput.innerHTML = 
-        `<div class = "prompt_hit">visitor@stupienius.Web:~$ </div>
+function inputPrompt(textarea, e) {
+  if (isAnimation) return;
+  if (/^[a-zA-Z]$/.test(e.key)) {
+    newInput.innerHTML = `<div class = "prompt_hit">visitor@stupienius.Web:~$ </div>
          <div class = "prompt_input">${textarea.value}${e.key}</div>
          <div class = "cursor"></div>`;
-    }else{
-        setTimeout(() => {
-            newInput.innerHTML = 
-            `<div class = "prompt_hit">visitor@stupienius.Web:~$ </div>
+  } else {
+    setTimeout(() => {
+      newInput.innerHTML = `<div class = "prompt_hit">visitor@stupienius.Web:~$ </div>
              <div class = "prompt_input">${textarea.value}</div>
              <div class = "cursor"></div>`;
-        }, 20);
-    }
+    }, 20);
+  }
 }
 
-
-function executeCommand(textarea,e){
-    if(isAnimation) return ;
-    if(e.key !== "Enter") return;
-    const command = textarea.value;
-    history.push(command);
-    switch (command.trim()){
-        case 'whoami':
-            addParagraphAnimation(whoami);
-            break;
-        case 'help':
-            addParagraphAnimation(help);
-            break;
-        case 'who':
-            addParagraphAnimation(who);
-            break;
-        case 'project':
-            addParagraphAnimation(project);
-            break;
-        case 'song':
-            addParagraphAnimation(song);
-            newTab("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb29s")
-            break;
-        case 'contact':
-            addParagraphAnimation(contact);
-            break;
-        case 'banner':
-            addParagraphAnimation(banner);
-            break;
-        case 'game':
-            addParagraphAnimation(game);
-            break;
-        case 'history':
-            addParagraphAnimation(history);
-            break;
-        case 'singer':
-            addParagraphAnimation(["Must be Post Malone!!!!"]);
-            newTab("https://www.postmalone.com/")
-            break;
-        case 'github':
-            addParagraphAnimation(["opening github..."]);
-            newTab("https://github.com/stupienius");
-            break;
-        case 'facebook':
-            addParagraphAnimation(["opening facebook..."]);
-            newTab("https://www.facebook.com/profile.php?id=100053935834116");
-            break;
-        case 'instagram':
-            addParagraphAnimation(["opening instagram..."]);
-            newTab("https://www.instagram.com/stupienius/");
-            break;
-        case 'x':
-            addParagraphAnimation(["opening x..."]);
-            newTab("https://twitter.com/stupienius");
-            break;
-        default:
-            addParagraphAnimation(["command not find"]);
-    }
+function executeCommand(textarea, e) {
+  if (isAnimation) return;
+  if (e.key !== "Enter") return;
+  const command = textarea.value;
+  history.push(command);
+  switch (command.trim()) {
+    case "whoami":
+      addParagraphAnimation(whoami);
+      break;
+    case "help":
+      addParagraphAnimation(help);
+      break;
+    case "who":
+      addParagraphAnimation(who);
+      break;
+    case "project":
+      addParagraphAnimation(project);
+      break;
+    case "song":
+      addParagraphAnimation(song);
+      newTab("https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb29s");
+      break;
+    case "contact":
+      addParagraphAnimation(contact);
+      break;
+    case "banner":
+      addParagraphAnimation(banner);
+      break;
+    case "game":
+      addParagraphAnimation(game);
+      break;
+    case "history":
+      addParagraphAnimation(history);
+      break;
+    case "singer":
+      addParagraphAnimation(["Must be Post Malone!!!!"]);
+      newTab("https://www.postmalone.com/");
+      break;
+    case "github":
+      addParagraphAnimation(["opening github..."]);
+      newTab("https://github.com/stupienius");
+      break;
+    case "facebook":
+      addParagraphAnimation(["opening facebook..."]);
+      newTab("https://www.facebook.com/profile.php?id=100053935834116");
+      break;
+    case "instagram":
+      addParagraphAnimation(["opening instagram..."]);
+      newTab("https://www.instagram.com/stupienius/");
+      break;
+    case "x":
+      addParagraphAnimation(["opening x..."]);
+      newTab("https://twitter.com/stupienius");
+      break;
+    default:
+      addParagraphAnimation(["command not find"]);
+  }
 }
 
-function toNextCommand(){
-    inputTextarea.value = "";
-    addNewCommandBlock();
+function toNextCommand() {
+  inputTextarea.value = "";
+  addNewCommandBlock();
 }
 
-function createOutputLine(text){
-    const outputLine = document.createElement('div');
-    outputLine.textContent = text;
-    return outputLine;
+function createOutputLine(text) {
+  const outputLine = document.createElement("div");
+  outputLine.textContent = text;
+  return outputLine;
 }
 
 async function addParagraphAnimation(text) {
-    removeCursor();
-    isAnimation = true;
-    for (const line of text) {
-      await addLineAnimation(line);
-    }
-    isAnimation = false;
-    toNextCommand();
+  removeCursor();
+  isAnimation = true;
+  for (const line of text) {
+    await addLineAnimation(line);
+  }
+  isAnimation = false;
+  toNextCommand();
 }
 
-async function addLineAnimation(text){                      
-    const p = document.createElement("p");
-    newOutput.appendChild(p);
-    let color = "";
-    let link = "";
-    let inLabel = false;
-    const a = document.createElement("a");
-    for (let i=0; i < text.length; i++) {
-        await new Promise(resolve => setTimeout(resolve, 1));
-        if(text.charAt(i) === " " && text.charAt(i + 1) === " "){
-            p.innerHTML += "&nbsp;&nbsp;";
-            i++;
-        }else{
-            if(text.charAt(i) === "("){                                //(class,href)<content>
-                i++;
-                for(;text.charAt(i) !== ",";i++){
-                    color += text.charAt(i);
-                }
-                i++;
-                for(;text.charAt(i) !== ")";i++){
-                    link += text.charAt(i);
-                }
-                i++;
-                p.appendChild(a);
-                a.classList.add(color);
-                if(link.length > 0){
-                    a.href = link;
-                    a.target = '_blank'
-                }
-            }
-            if(text.charAt(i) === "<" || text.charAt(i) === ">"){
-                inLabel = !inLabel;
-                i++; 
-            }
-            if(inLabel){
-                a.innerHTML += text.charAt(i);
-            }else{
-                p.innerHTML += text.charAt(i);
-            }
+async function addLineAnimation(text) {
+  const p = document.createElement("p");
+  newOutput.appendChild(p);
+  let color = "";
+  let link = "";
+  let inLabel = false;
+  const a = document.createElement("a");
+  for (let i = 0; i < text.length; i++) {
+    await new Promise((resolve) => setTimeout(resolve, 1));
+    if (text.charAt(i) === " " && text.charAt(i + 1) === " ") {
+      p.innerHTML += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      if (text.charAt(i) === "(") {
+        //(class,link)<content>
+        i++;
+        for (; text.charAt(i) !== ","; i++) {
+          color += text.charAt(i);
         }
+        i++;
+        for (; text.charAt(i) !== ")"; i++) {
+          link += text.charAt(i);
+        }
+        i++;
+        p.appendChild(a);
+        a.classList.add(color);
+        if (link.length > 0) {
+          a.href = link;
+          a.target = "_blank";
+        }
+      }
+      if (text.charAt(i) === "<" || text.charAt(i) === ">") {
+        inLabel = !inLabel;
+        i++;
+      }
+      if (inLabel) {
+        a.innerHTML += text.charAt(i);
+      } else {
+        p.innerHTML += text.charAt(i);
+      }
+      window.scrollTo(0, document.body.offsetHeight);
     }
-    p.innerHTML += "<br>";
-    window.scrollTo(0, document.body.offsetHeight);
+  }
+  p.innerHTML += "<br>";
 }
 
-function newTab(link){
-    setTimeout(function() {
-      window.open(link, "_blank");
-    }, 500);
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 1500);
 }
 
+function addNewCommandBlock() {
+  newCommand = null;
+  newInput = null;
+  newOutput = null;
+  container = null;
 
-function addNewCommandBlock(){
-    
-    newCommand = null;
-    newInput = null;
-    newOutput = null;
-    container = null;
+  newCommand = document.createElement("div");
+  newInput = document.createElement("div");
+  newOutput = document.createElement("div");
 
-    newCommand = document.createElement('div');
-    newInput = document.createElement('div');
-    newOutput = document.createElement('div');
+  container = terminl.appendChild(newCommand);
+  newCommand.classList.add("container");
 
-    container = terminl.appendChild(newCommand);
-    newCommand.classList.add('container');
-    
-    container.appendChild(newInput);
-    newInput.classList.add('input');
-    
-    container.appendChild(newOutput);
-    newOutput.classList.add('output');
+  container.appendChild(newInput);
+  newInput.classList.add("input");
 
-    newInput.innerHTML =
-    `<div class = "prompt_hit">visitor@stupienius.Web:~$ </div>
-     <div class = "cursor"></div>`
+  container.appendChild(newOutput);
+  newOutput.classList.add("output");
 
-     
-    window.scrollTo(0, document.body.offsetHeight);
-    inputTextarea.style.top = `${document.body.offsetHeight}px`
-    console.log(inputTextarea.style.top)
+  newInput.innerHTML = `<div class = "prompt_hit">visitor@stupienius.Web:~$ </div>
+     <div class = "cursor"></div>`;
+
+  window.scrollTo(0, document.body.offsetHeight);
+  inputTextarea.style.top = `${document.body.offsetHeight}px`;
+  console.log(inputTextarea.style.top);
 }
 
-function removeCursor(){
-    const cursor = document.querySelectorAll(".cursor");
-    cursor.forEach(e => {e.style.display = "none";});
+function removeCursor() {
+  const cursor = document.querySelectorAll(".cursor");
+  cursor.forEach((e) => {
+    e.style.display = "none";
+  });
 }
